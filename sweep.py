ce7cd912b31fe94cc9f40a0548b95a97ddd88215
Darrell Silver <darrell@darrellsilver.com>
2013-11-16 15:33:31 -0500

    add autoloading of subclasses in scope via SWEEP_TASK_LIBS env var

--- sweep.py
+++ sweep.py
@@ -3,10 +3,24 @@
 Cmd-line interface to *sweep* the system, running tasks!
 """
 
-def main():
+import os
+from datetime import datetime
+from sweeps.models import AbstractTask
+
+def _get_subclasses():
+    # This lib doesn't know about subclasses unless they're loaded
+    # use this env var to find them.
+    task_classes = os.environ.get('SWEEP_TASK_LIBS', None)
+    if task_classes:
+        for tc in task_classes.split(','):
+            __import__(tc)
+
+def sweep():
+    _get_subclasses()
     asof = datetime.utcnow()
-    for task in AbstractTask.__subclasses__():
-        task.run_all(asof)
+    for task_class in AbstractTask.__subclasses__():
+        for task in task_class.query.all():
+            task.sweep(asof)
 
 if __name__ == '__main__':
-    main()
+    sweep()
